16dbbdaa5b916475903ace25223192b9ed4c98de
abhi10001 <abhilash.reddy.sr@gmail.com>
2020-10-16 00:55:48 -0500

    Requesting permissions for the contacts has been setup

--- app/src/main/java/com/android/project/contacts/MainActivity.kt
+++ app/src/main/java/com/android/project/contacts/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.android.project.contacts
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil.setContentView
+import com.android.project.contacts.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    val PERMISSION_REQUEST_CODE = 100
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = setContentView(this, R.layout.activity_main)
+        checkPermission(Manifest.permission.READ_CONTACTS, PERMISSION_REQUEST_CODE)
+    }
+
+
+    fun checkPermission(permission: String, requestCode: Int) {
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        }else{
+           // readContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //permissions granted
+            }
+            else {
+                //basically looping in again to ask permission for now
+                checkPermission(Manifest.permission.READ_CONTACTS, PERMISSION_REQUEST_CODE)
+            }
+        }
     }
 }
